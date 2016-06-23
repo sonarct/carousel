b8c0717be3777918b747ecdc506feb99cfdd8ba1
sonarct <sonarct@gmail.com>
2016-06-23 14:19:03 +0300

    add matrix transformations

--- Divificator.js
+++ Divificator.js
@@ -15,11 +15,11 @@ function Divificator(amount) {
 		// This function generates vibrant, "evenly spaced" colours (i.e. no clustering). This is ideal for creating easily distinguishable vibrant markers in Google Maps and other apps.
 		// Adam Cole, 2011-Sept-14
 		// HSV to RBG adapted from: http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
-		var r, g, b;
-		var h = step / numOfSteps;
-		var i = ~~(h * 6);
-		var f = h * 6 - i;
-		var q = 1 - f;
+		var r, g, b
+		,	h = step / numOfSteps
+		,	i = ~~(h * 6)
+		,	f = h * 6 - i
+		,	q = 1 - f
 		switch(i % 6){
 			case 0: r = 1; g = f; b = 0; break;
 			case 1: r = q; g = 1; b = 0; break;
@@ -28,8 +28,8 @@ function Divificator(amount) {
 			case 4: r = f; g = 0; b = 1; break;
 			case 5: r = 1; g = 0; b = q; break;
 		}
-		var c = "#" + ("00" + (~ ~(r * 255)).toString(16)).slice(-2) + ("00" + (~ ~(g * 255)).toString(16)).slice(-2) + ("00" + (~ ~(b * 255)).toString(16)).slice(-2);
-		return (c);
+		var c = "#" + ("00" + (~ ~(r * 255)).toString(16)).slice(-2) + ("00" + (~ ~(g * 255)).toString(16)).slice(-2) + ("00" + (~ ~(b * 255)).toString(16)).slice(-2)
+		return (c)
 	}
 
 
--- control.js
+++ control.js
@@ -4,12 +4,13 @@ var	x0 		= window.innerWidth / 2
 ,	radiusY = 50
 ,	angle 	= 0
 ,	speed	= 0.005
-,	amount  = 200
+,	amount  = 20
 
 var myDrag 		= new Drag(document)
 ,	myMomentum 	= new Momentum
 ,	myDivs 		= new Divificator(amount);
 
+var result
 
 function animate() {
 	myMomentum.push(myDrag.offset)
@@ -19,7 +20,7 @@ function animate() {
 	}
 	calculateAngle()
 	for (var i = 0; i < amount; i++) {
-		drawObject(myDivs.div[i], angleOffset(i / amount))
+		drawObject(myDivs.div[i], angleOffset(i / amount), 1)
 	}
 	requestAnimationFrame(animate)
 }
@@ -37,14 +38,40 @@ function drawObject(div, angleOffset, diffHeight) {
 	,	y = y0 + radiusY * Math.sin(angle + angleOffset)
 	,	z = z0 + diffHeight
 
+	var coordinates = [[x, y, z, 1]]
+
+	var matrix = [[1,1,0,0],[1,0,0,0],[0,0,1,0],[0,0,0,1]]
+
+	result = MultiplyMatrix(coordinates, matrix)
+
+
+
 	var scale = Math.abs(Math.sin((angle + angleOffset + Math.PI / 2) / 2))
 	if(scale < 0.4) scale = 0.4
 	var opacity = Math.abs(Math.sin((angle + angleOffset + Math.PI / 2) / 2))
 	if(opacity < 0.99) opacity = Math.pow(opacity, 3)
 
-	div.style.zIndex = Math.round(y)
-	div.style.transform = 'translate(' + x + 'px,' + y + 'px) scale(' + scale + ')'
+	div.style.zIndex = Math.round(result[0][1])
+	div.style.transform = 'translate(' + result[0][0] + 'px,' + result[0][1] + 'px) scale(' + scale + ')'
 	div.style.opacity = opacity
+}
+
+
+function MultiplyMatrix(A,B) {
+    var rowsA = A.length, colsA = A[0].length,
+        rowsB = B.length, colsB = B[0].length,
+        C = [];
+
+    if (colsA != rowsB) return false;
+    for (var i = 0; i < rowsA; i++) C[i] = [];
+    for (var k = 0; k < colsB; k++)
+     { for (var i = 0; i < rowsA; i++)
+        { var t = 0;
+          for (var j = 0; j < rowsB; j++) t += A[i][j]*B[j][k];
+          C[i][k] = t;
+        }
+     }
+    return C;
 }
 
 
@@ -69,3 +96,9 @@ myDrag.events.on('drag', function() {
 })
 
 
+
+// var coordinates = [[3, 3, 3, 1]]
+
+// 	var matrix = [[1,0,0,0],[0,0,0,0],[0,0,1,0],[0,0,0,1]]
+
+// var xxx =	MultiplyMatrix(coordinates, matrix)
